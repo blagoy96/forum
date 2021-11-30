f4d2292d21cf0bed48dc2291be7d8a09e91a8a6a
blagoy96 <blagoy.stoitsov@gmail.com>
2021-11-30 17:44:21 +0200

    search bar

--- client/src/components/Header/Demo/Demo.js
+++ client/src/components/Header/Demo/Demo.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 const Demo = (props) => {
+  const [state, setState] = useState({});
+  let isFirst = true;
   const searchHandler = (e) => {
     e.preventDefault();
 
     props.posts.forEach((element) => {
-      if (element.title === e.target.input.value) {
-        console.log(element.title);
+      if (element.title === e.target.input.value && isFirst === true) {
+        return fetch(`http://localhost:5000/posts?title=${element.title}`)
+          .then((res) => res.json())
+          .then((res) => setState(res));
       }
+      isFirst = false;
     });
+    console.log(state);
   };
 
   return (
