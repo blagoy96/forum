e66ca8c1c74d3c0ce90ed5a057adca89a409655b
blagoy96 <blagoy.stoitsov@gmail.com>
2021-12-15 16:34:21 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -9,24 +9,28 @@ import Footer from "./components/Footer/Footer";
 import Login from "./components/Header/Login/Login";
 import Register from "./components/Header/Register/Register";
 import CreatePost from "./components/Main/CreatePost/CreatePost";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { db } from "./config/firebase";
 import { collection, getDocs } from "firebase/firestore";
+export const PostsContext = React.createContext([]);
 const App = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     getDocs(collection(db, "posts")).then((snapShot) =>
       snapShot.forEach((doc) => {
-        posts.push(doc.data());
+        setPosts((result) => [...result, doc.data()]);
       })
     );
   }, []);
-  console.log(posts);
+
+  console.log(posts.length);
   return (
     <>
       <Navigation />
+
       <Routes>
+        {/* <PostsContext.Provider value={posts}> */}
         <Route path="/" element={<Posts posts={posts} />} />
         <Route path="/about-us" element={<About />} />
         <Route path="/create-post" element={<CreatePost />} />
@@ -36,7 +40,9 @@ const App = () => {
         <Route path="/logout" />
         <Route path="/demo" element={<Demo posts={posts} />} />
         <Route path="*" element={<h1>Error Page</h1>}></Route>
+        {/* </PostsContext.Provider> */}
       </Routes>
+
       <Footer />
     </>
   );
--- client/src/components/Main/CreatePost/CreatePost.js
+++ client/src/components/Main/CreatePost/CreatePost.js
@@ -2,8 +2,11 @@ import "./CreatePost.css";
 import { db } from "../../../config/firebase";
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { PostsContext } from "../../../App";
 
 const CreatePost = () => {
+  const context = useContext(PostsContext);
   let navigate = useNavigate();
   const onSubmit = (e) => {
     e.preventDefault();
@@ -16,30 +19,31 @@ const CreatePost = () => {
         e.target.title.value +
         e.target.description.value,
     };
-
-    setDoc(doc(db, "posts", `${e.target.username.value}`), post).then(
-      navigate("/")
-    );
+    setDoc(doc(db, "posts", `${e.target.username.value}`), post)
+      .then(PostsContext(post))
+      .then(navigate("/"));
   };
 
   return (
-    <div>
-      <h1>Create Post</h1>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" />
-        <input type="text" name="title" placeholder="Title" />
-        <textarea
-          type="text"
-          rows="20"
-          cols="100"
-          name="description"
-          placeholder="Write here...."
-        />
+    <PostsContext.Consumer>
+      <div>
+        <h1>Create Post</h1>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" />
+          <input type="text" name="title" placeholder="Title" />
+          <textarea
+            type="text"
+            rows="20"
+            cols="100"
+            name="description"
+            placeholder="Write here...."
+          />
 
-        <input type="submit" name="submit" value="Send" />
-      </form>
-    </div>
+          <input type="submit" name="submit" value="Send" />
+        </form>
+      </div>
+    </PostsContext.Consumer>
   );
 };
 
