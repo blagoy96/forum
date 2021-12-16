c3ae3095376c938cd918fcbfdd93fb3fc499212e
blagoy96 <blagoy.stoitsov@gmail.com>
2021-12-16 13:01:09 +0200

    clear search bar

--- client/src/components/Header/Demo/Demo.js
+++ client/src/components/Header/Demo/Demo.js
@@ -1,20 +1,9 @@
 import { useState } from "react";
 const Demo = (props) => {
   const [state, setState] = useState({});
-  let isFirst = true;
   const searchHandler = (e) => {
-    e.preventDefault();
-
-    props.posts.forEach((element) => {
-      if (element.title === e.target.input.value && isFirst === true) {
-        return fetch(`http://localhost:5000/posts?title=${element.title}`)
-          .then((res) => res.json())
-          .then((res) => setState(res));
-      }
-      isFirst = false;
-    });
-    console.log(state);
-  };
+    
+  }
 
   return (
     <form onSubmit={searchHandler}>
