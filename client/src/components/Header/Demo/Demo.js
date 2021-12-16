533e19370ebdc2b1cd0be4fe4a08d40ec80e4169
blagoy96 <blagoy.stoitsov@gmail.com>
2021-12-16 16:57:53 +0200

    initialize searching

--- client/src/components/Header/Demo/Demo.js
+++ client/src/components/Header/Demo/Demo.js
@@ -1,15 +1,38 @@
 import { useState } from "react";
+import Posts from "../Posts/Posts";
 const Demo = (props) => {
-  const [state, setState] = useState({});
+  const [state, setState] = useState([]);
+  const [count, setCount] = useState(0);
   const searchHandler = (e) => {
-    
-  }
+    e.preventDefault();
+    setState(() => []);
+    props.posts.filter((value) => {
+      if (value.title.toLowerCase() === e.target.input.value.toLowerCase()) {
+        setCount((oldState) => (oldState += 1));
+        setState((res) => [...res, value]);
+      }
+    });
+
+    console.log(count);
+    e.target.input.value = null;
+
+    // props.posts.forEach((element) => {
+    //   if (element.title === e.target.input.value) {
+    //     setState((result) => [...result, element]);
+    //   }
+    // });
+    // e.target.input.value = null;
+    // console.log(state);
+  };
 
   return (
-    <form onSubmit={searchHandler}>
-      <input type="text" name="input" />
-      <input type="submit" value="search" />
-    </form>
+    <>
+      <Posts posts={state} />
+      <form onSubmit={searchHandler}>
+        <input type="text" name="input" />
+        <input type="submit" value="search" />
+      </form>
+    </>
   );
 };
 
